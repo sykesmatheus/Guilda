45d1777dbaa44d863c99768879143fd3630b5c60
Lucas Silva <124575276+Lucas07silva@users.noreply.github.com>
2026-02-23 09:36:26 -0300

    Criação do Nucleo de Usuários e do Catálogo de Jogos e Conteúdo
    Added tables for user management, friendships, groups, notifications, games, and system requirements.

--- tabelas.sql
+++ tabelas.sql
@@ -104,3 +104,136 @@ motivo_banimento text,
 data_expiracao date
 ); 
 >>>>>>> 2730d8f5e5e1dbcd36e5cba29cd8beac6c9da799
+
+
+-- Nucleo de usuario
+	
+-- Tabela de Usuários
+create table usuarios (
+	id int not null,
+	email varchar(150) not null,
+	senha_hash varchar(150) not null,
+	data_criacao datatime not null,
+	saldo_carteira decimal(10,2) default,
+	primary key (id)
+)
+
+-- Tabela de Perfis
+create table perfis (
+	id_usuarios int not null,
+	nick_name varchar(50) not null,
+	bio varchar(250),
+	avatar_url varchar (2083),
+	nivel_atual int default 1,
+)
+
+-- Tabela de Amizades
+create table amizades (
+	id_usuario_a int not null
+	id_usuario_b int not null
+	data_inicio datatime not null,
+	status varchar(20) not null
+	primary key (id_usuario_a, id_usuario_b)
+)
+-- Tabela de Lista de Bloqueio
+create table lista_bloqueio (
+	id_usuario int not null,
+	id_usuario_bloqueado int not null,
+	primary key (id_usuario, id_usuario_bloqueado)
+)
+
+-- Tabela de Grupos
+create table grupos (
+    id int not null,
+    nome varchar(100) not null,
+    descricao varchar(255),
+    id_criador int not null,
+    primary key (id)
+);
+
+-- Tabela de Membros do Grupo
+create table membros_grupo (
+    id_grupo int not null,
+    id_usuario int not null,
+    cargo varchar(50) not null,
+    primary key (id_grupo, id_usuario)
+);
+
+-- Tabela de Notificacoes
+create table notificacoes (
+    id int not null,
+    id_usuario int not null,
+    mensagem varchar(255) not null,
+    lida_bool boolean,
+    data datetime not null,
+    primary key (id)
+);
+
+-- Fim nucleo de Usuarios
+
+
+-- Catálogo de Jogos e Conteúdo
+
+
+
+
+-- Tabela de Tags
+create table tags (
+    id int not null,
+    nome_tag varchar(50) not null,
+    primary key (id)
+);
+
+-- Tabela de Jogos_Tags (Relacionamento N:N)
+create table jogos_tags (
+    id_jogo int not null,
+    id_tag int not null,
+    numero_votos int default 0,
+    primary key (id_jogo, id_tag)
+);
+
+-- Tabela de Idiomas
+create table idiomas (
+    id int not null,
+    nome_idioma varchar(50) not null,
+    primary key (id)
+);
+
+-- Tabela de Jogos_Idiomas
+create table jogos_idiomas (
+    id_jogo int not null,
+    id_idioma int not null,
+    dublado_bool boolean,
+    legendado_bool boolean,
+    primary key (id_jogo, id_idioma)
+);
+
+-- Tabela de Requisitos_Sistema
+create table requisitos_sistema (
+    id int not null,
+    id_jogo int not null,
+    tipo varchar(10) not null, -- 'Min' ou 'Rec'
+    cpu varchar(100),
+    gpu varchar(100),
+    ram varchar(20),
+    os varchar(50),
+    primary key (id)
+);
+-- Fim do Catálogo de Jogos e Conteúdo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

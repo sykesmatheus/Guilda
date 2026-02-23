5dda9e43e0b24416c16ced87739de2263bb4271e
Gabriel Lima <limagabriel3926u@gmail.com>
2026-02-23 12:26:27 -0300

    Update 03-loja-financeiro.sql

--- ddl/03-loja-financeiro.sql
+++ ddl/03-loja-financeiro.sql
@@ -1,1 +1,59 @@
+create table moedas (
+	id_moeda int not null,
+	codigo_moeda varchar(3) not null ,
+	simbolo_moeda varchar(5),
+	primary key (id_moeda)
+);
 
+create table precos_regionais (
+	id_jogo int not null,
+	id_moeda int not null,
+	valor_localizado decimal (10,2),
+	primary key (id_jogo, id_moeda)
+);
+
+create table metodos_pagamento (
+	id_metodo int not null,
+	nome_metodo varchar(50) not null,
+	primary key (id_metodo)
+);
+
+create table transacoes (
+	id_transacao int not null,
+	id_usuario int not null,
+	id_metodo int not null ,
+	valor_total decimal(10,2),
+	data_transacao timestamp,
+	status_transacao varchar(20),
+	primary key (id_transacao)
+);
+
+create table itens_transacao(
+	id_transacao int not null,
+	id_jogo int not null,
+	preco_vendido decimal(10,2),
+	primary key (id_transacao, id_jogo)
+);
+
+create table lista_desejo(
+	id_usuario int not null,
+	id_jogo int not null,
+	data_adicao date,
+	primary key (id_usuario, id_jogo)
+);
+
+create table cupons (
+	id_cupom int not null,
+	codigo_cupom varchar(20) not null,
+	desconto_percentual int,
+	id_usuario_dono int not null,
+	primary key (id_cupom)
+);
+
+create table reembolso (
+	id_reembolso int not null,
+	id_transacao int not null,
+	motivo_reembolso text,
+	status_aprovacao varchar(20),
+	primary key (id_reembolso)
+);

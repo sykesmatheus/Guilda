0b5847a0c93953f2579de7a87ee5b2a9f43fcdc3
vitor <103426211+Oya2547@users.noreply.github.com>
2026-02-25 19:29:30 -0300

    Fix SQL syntax and update table definitions

--- ddl/01-nucleo-usuarios.sql
+++ ddl/01-nucleo-usuarios.sql
@@ -6,31 +6,34 @@ create table usuarios (
 	data_criacao datatime not null,
 	saldo_carteira decimal(10,2) default,
 	primary key (id)
-)
+);
 
 -- Tabela de Perfis
 create table perfis (
-	id_usuarios int not null,
+	id int not null,
 	nick_name varchar(50) not null,
 	bio varchar(250),
 	avatar_url varchar (2083),
 	nivel_atual int default 1,
-)
+	primary key (id)
+);
 
 -- Tabela de Amizades
 create table amizades (
-	id_usuario_a int not null
-	id_usuario_b int not null
+	id int not null,
+	id_a int not null
+	id_b int not null
 	data_inicio datatime not null,
 	status varchar(20) not null
-	primary key (id_usuario_a, id_usuario_b)
-)
+	primary key (id)
+);
 -- Tabela de Lista de Bloqueio
-create table lista_bloqueio (
+create table listas_bloqueio (
+	id int not null,
 	id_usuario int not null,
 	id_usuario_bloqueado int not null,
-	primary key (id_usuario, id_usuario_bloqueado)
-)
+	primary key (id)
+);
 
 -- Tabela de Grupos
 create table grupos (
@@ -43,10 +46,11 @@ create table grupos (
 
 -- Tabela de Membros do Grupo
 create table membros_grupo (
+	id int not null,
     id_grupo int not null,
     id_usuario int not null,
     cargo varchar(50) not null,
-    primary key (id_grupo, id_usuario)
+    primary key (id)
 );
 
 -- Tabela de Notificacoes
